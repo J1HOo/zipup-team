010b6d2c4e5b27a3ab18dd850215710f1950d011
jahn199 <anj575981@gmail.com>
2025-01-07 16:48:10 +0900

    정규식
    수정

--- src/main/resources/static/js/signUp.js
+++ src/main/resources/static/js/signUp.js
@@ -95,7 +95,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".signup-form");
     const checkEmailBtn = document.getElementById("check-selleremail-btn");
 
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z.-]+\.com$/;
     const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*+=-])[A-Za-z\d!@#$%^&*+=-]{8,}$/;
 
     checkEmailBtn.addEventListener("click", function () {
